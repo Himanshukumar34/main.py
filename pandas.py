35c727afc11a8c2cc9bd2a450de7bbfb34eaba46
himanshu rajput <gourirajput553@gmail.com>
2026-01-03 22:06:53 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,16 +1,39 @@
-def student():
+num1 = []
+
+while True:
     try:
-        name=str(input("Enter your name :"))
-        num=int(input("Enter your no. subject  :"))
-        marks_list=[]
-        for i in range(num+1):
-            marks=int(input("Enter your marks :"))
-            marks_list.append(marks)
-        with open("studnt.txt","a") as f:
-            f.write(name+"\n") 
-            f.write("Marks: " + ",".join(map(str, marks_list)) + "\n")
-        print("your recorded ")
+        print("------- Todo List ------")
+        print("1. Add task")
+        print("2. Remove task")
+        print("3. View tasks")
+        print("4. Exit")
+
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            task = input("Enter your task: ")
+            num1.append(task)
+            print("Your task is successfully added")
+
+        elif choice == 2:
+            task2 = input("Enter task to remove: ")
+            if task2 in num1:
+                num1.remove(task2)
+                print("Task removed successfully")
+            else:
+                print("Task not found")
+
+        elif choice == 3:
+            print("Your tasks are:", num1)
+
+        elif choice == 4:
+            print("Thank you")
+            break
+
+        else:
+            print("Invalid choice")
+
     except ValueError:
-        print("invalid input")
+        print("Your value is wrong")
 
             
