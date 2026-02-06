ff7082ff5e2d6a0e3e44fc19c52285b46607302c
himanshu rajput <gourirajput553@gmail.com>
2026-02-06 23:18:09 +0530

    Create tic_tac_toe.py

---
+++ tic_tac_toe.py
@@ -0,0 +1,11 @@
+# tic tac toe game 
+while True:
+    print("tic tac toe game play with himanshu")
+    print("   |    |   ")
+    print("------------")
+    print("   |    |   ")
+    print("------------")
+    print("   |    |   ")
+    cross=str(input("Enter your choice (X,O ):"))
+    choice=int(input("Enter your choice (1,2,3,4,5,6,7,8,9) :"))
+    if choice ==
