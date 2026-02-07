3ad530b7102e217cf0318b8bf7b13ef91f956059
himanshu rajput <gourirajput553@gmail.com>
2026-02-07 22:55:39 +0530

    jlo

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,11 +1,36 @@
-# tic tac toe game 
+import mysql.connector
+mydb=mysql.connector.connect(
+    host="localhost",
+    password="12qwQW?@",
+    database="contact",
+    user="root"
+)
+print("connected")
+myconn=mydb.cursor()
+myconn.execute("""
+     CREATE TABLE krishna(
+               id INT PRIMARY KEY,
+               name VARCHAR(255),
+               phone INT NOT NULL,
+               Email VARCHAR(255),
+               Address VARCHAR(255)
+);""")
+
 while True:
-    print("tic tac toe game play with himanshu")
-    print("   |    |   ")
-    print("------------")
-    print("   |    |   ")
-    print("------------")
-    print("   |    |   ")
-    cross=str(input("Enter your choice (X,O ):"))
-    choice=int(input("Enter your choice (1,2,3,4,5,6,7,8,9) :"))
-    if choice ==
+    print("1. Add contact")
+    print("2. View contact")
+    print("3. search contact")
+    print("4. update contact ")
+    print("5. delete contact")
+    choice=int(input("Enter your choice  :"))
+    if choice ==1:
+         name=str(input("Enter your name :")).strip()
+         phone =int(input("Enter your phone number :")).strip()
+         email=input("Enter your email :").strip()
+         address=str(input("Enter your address :")).strip()
+         num2="INSERT INTO krishna(name,phone,Email,Address), VALUES(%s,%s,%s,%s)"
+         num1=(name,phone,email,address)
+         myconn.execute(num2,num1)
+         mydb.commit()
+         print("your task is successfully created ")
+        
