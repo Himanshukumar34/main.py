21eccda25d62de23916de1dce41d5cfc621a6484
himanshu rajput <gourirajput553@gmail.com>
2026-02-11 16:47:01 +0530

    Create pratcice.py

---
+++ pratcice.py
@@ -0,0 +1,20 @@
+# # table generator
+# num1=int(input("Enter your num1  :"))
+# for i in range(1,11):
+#     num3=num1*i
+#     print(num1,"*",i,"=",num3)
+
+# # sum for N natural number 
+# user=int(input("Enter your sum no :"))
+# num1=user*(user+1)/2
+# print(num1)]
+
+# EVEN NUMBERS FORM N TERMS
+# user=int(input("Enter your num1:"))
+# for i in range(1,user+1):
+#     num1=i%2
+#     if num1==0:
+#         print(i)
+
+
+    
