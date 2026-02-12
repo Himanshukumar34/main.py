77589e34cfcb666e6f8e608aafe41b6b7ef661a4
himanshu rajput <gourirajput553@gmail.com>
2026-02-12 16:56:58 +0530

    Update pratcice.py

--- pratcice.py
+++ pratcice.py
@@ -1,20 +1,49 @@
-# # table generator
-# num1=int(input("Enter your num1  :"))
-# for i in range(1,11):
-#     num3=num1*i
-#     print(num1,"*",i,"=",num3)
+# Add student
+# View all students
+# Search student
+# Update marks
+# Delete student
+# Show topper
+# Exit
+num1=[]
+def add_student():
+    student=str(input("Enter student name :"))
+    roll_num=int(input("Enter your roll num :"))
+    marks1=int(input("Enter your marks in hindi:"))
+    marks2=int(input("Enter your marks in english :"))
+    marks3=int(input("Enter your student in math :"))
+    num1.append(f"""Name= {student}
+hindi= {marks1}
+english= {marks2} 
+math=  { marks3}
+Roll= {roll_num}""")
+    print("record saved in data ")
+def view_all_student():
+    if not num1:
+        print("NO found student record")
+    else:
+        print("----STUDENT RECORD-----")
+        for i in num1:
+            print(i)
+        
 
-# # sum for N natural number 
-# user=int(input("Enter your sum no :"))
-# num1=user*(user+1)/2
-# print(num1)]
+# def search_student():
+#     num2=int(input("Enter roll num which you want to see:"))
+#     for i in num1:
+#         if i.strip()==f"Roll={num2}":
+#             print(num1[0])
+while True:
+    print("---------student management system ------")
+    print("1. Add student")
+    print("2. View student")
+    print("3.search student")
+    print("4.update student")
+    print("5.delete student")
+    print("6.show student")
+    print("7.Exit ")
+    choice=int(input("Enter your choice ;"))
+    if choice==1:
+        add_student()
+    elif choice==2:
+        view_all_student()
 
-# EVEN NUMBERS FORM N TERMS
-# user=int(input("Enter your num1:"))
-# for i in range(1,user+1):
-#     num1=i%2
-#     if num1==0:
-#         print(i)
-
-
-    
