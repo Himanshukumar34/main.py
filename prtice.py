dc8de2f76dd2390a13768e497d385b7283513ba5
himanshu rajput <gourirajput553@gmail.com>
2026-02-19 23:35:22 +0530

    Update prtice.py

--- prtice.py
+++ prtice.py
@@ -1,10 +1,258 @@
-num1=int(input("Enter your num1:"))
-num2=int(input("Enter your num2: "))
-num3=int(input("Enter your num3:"))
-if num1>num2 and num1>num3:
-    print(f"max={num1}")
-elif num2>num3 and num2> num1:
-    print(f"max={num2}")
+import random 
+import statistics
+num1=random.randint(1,100)
+num2=random.randint(1,100)
+num3=random.randint(1,100)
+num4=random.randint(1,100)
+num5=random.randint(1,100)
+num6=random.randint(1,100)
+n=num1,num2,num3,num4,num5,num6
+print(statistics.mean(n))
+print(statistics.median(n))
+print(statistics.mode(n))
 
-elif num3>num1 and num3>num2:
-    print(f"max={num3}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # num1=int(input("Enter your num1: "))
+# # num2=int(input("Enter your num2 :"))
+
+# # operator=str(input("Enter your operatore(+,-,*,/) :"))
+# # if operator=="+":
+# #     print("add",num1+num2)
+# # elif operator=="-":
+# #     print("sub",num1-num2)
+# # elif operator=="*":
+# #     print("mult",num1*num2)
+# # elif operator=="/":
+# #     print("div",num1/num2)
+# # elif operator=="%":
+# #     print("modulus",num1%num2)
+# # elif operator=="//":
+# #     print("floor",num1//num2)
+
+# # # project-02
+# # x=int(int(input("Enter your x:")))
+# # y=int(input("Enter your y: "))
+# # z=int(input("Enter your z:"))
+# # solve=(x*y+y*z+z*x)/(x*y*z)
+# # print(solve)
+
+# # project-03
+# # num1=str(input("Enter your letter:"))
+# # times=int(input("Enter your times :"))
+# # for i in range(0,times):
+# #     print(num1)
+
+# # project-04
+# # cels=int(input("Enter your cels: "))
+# # fehrn=cels*(9/5)+32
+# # # print(fehrn)
+
+# # # project-05
+# # import random
+# # count=0
+# # for i in range(1,4):
+# #    num1=random.randint(1,100)
+# #    count+=num1
+# # print(count)
+
+
