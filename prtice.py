eaba91b46dc1120ec6dfde8913ae90ae0e8acc42
himanshu rajput <gourirajput553@gmail.com>
2026-02-20 23:01:14 +0530

    Update prtice.py

--- prtice.py
+++ prtice.py
@@ -1,18 +1,50 @@
-import random 
-import statistics
-num1=random.randint(1,100)
-num2=random.randint(1,100)
-num3=random.randint(1,100)
-num4=random.randint(1,100)
-num5=random.randint(1,100)
-num6=random.randint(1,100)
-n=num1,num2,num3,num4,num5,num6
-print(statistics.mean(n))
-print(statistics.median(n))
-print(statistics.mode(n))
+# import random 
+# import statistics
+# num1=random.randint(1,100)
+# num2=random.randint(1,100)
+# num3=random.randint(1,100)
+# num4=random.randint(1,100)
+# num5=random.randint(1,100)
+# num6=random.randint(1,100)
+# n=num1,num2,num3,num4,num5,num6
+# print(statistics.mean(n))
+# print(statistics.median(n))
+# # print(statistics.mode(n))
 
+# num1=str(input("Enter your str :"))
+# count=""
+# for i in num1:
+#     count=i+count
+# if count==num1:
+#     print("it is palindorm enumber ")
+# else:
+#     print("it is not a palin dome number")
 
+# check vowels and cononants 
+# num1=str(input("Enter you name:"))
+# vowels="a,e,i,o,u"
+# count=0
+# count2=0
+# for i in num1:
+#     if i in vowels:
+#         count+=1
+#     else:
+#         count2+=1
+# print("vowels",count)
+# print("consonant",count2)
 
+num1=input("Enter your you want: ")
+count=0
+count2=0
+count3=0
+for i in num1:
+    if i.isupper():
+        count+=1
+    elif i.lower():
+        count2+=1
+    else:
+        count3+=1
+print("uppercase",count ,"/n","lowercase")
 
 
 
@@ -210,49 +242,47 @@ print(statistics.mode(n))
 
 
 
+# # # num1=int(input("Enter your num1: "))
+# # # num2=int(input("Enter your num2 :"))
 
+# # # operator=str(input("Enter your operatore(+,-,*,/) :"))
+# # # if operator=="+":
+# # #     print("add",num1+num2)
+# # # elif operator=="-":
+# # #     print("sub",num1-num2)
+# # # elif operator=="*":
+# # #     print("mult",num1*num2)
+# # # elif operator=="/":
+# # #     print("div",num1/num2)
+# # # elif operator=="%":
+# # #     print("modulus",num1%num2)
+# # # elif operator=="//":
+# # #     print("floor",num1//num2)
 
-# # num1=int(input("Enter your num1: "))
-# # num2=int(input("Enter your num2 :"))
+# # # # project-02
+# # # x=int(int(input("Enter your x:")))
+# # # y=int(input("Enter your y: "))
+# # # z=int(input("Enter your z:"))
+# # # solve=(x*y+y*z+z*x)/(x*y*z)
+# # # print(solve)
 
-# # operator=str(input("Enter your operatore(+,-,*,/) :"))
-# # if operator=="+":
-# #     print("add",num1+num2)
-# # elif operator=="-":
-# #     print("sub",num1-num2)
-# # elif operator=="*":
-# #     print("mult",num1*num2)
-# # elif operator=="/":
-# #     print("div",num1/num2)
-# # elif operator=="%":
-# #     print("modulus",num1%num2)
-# # elif operator=="//":
-# #     print("floor",num1//num2)
+# # # project-03
+# # # num1=str(input("Enter your letter:"))
+# # # times=int(input("Enter your times :"))
+# # # for i in range(0,times):
+# # #     print(num1)
 
-# # # project-02
-# # x=int(int(input("Enter your x:")))
-# # y=int(input("Enter your y: "))
-# # z=int(input("Enter your z:"))
-# # solve=(x*y+y*z+z*x)/(x*y*z)
-# # print(solve)
+# # # project-04
+# # # cels=int(input("Enter your cels: "))
+# # # fehrn=cels*(9/5)+32
+# # # # print(fehrn)
 
-# # project-03
-# # num1=str(input("Enter your letter:"))
-# # times=int(input("Enter your times :"))
-# # for i in range(0,times):
-# #     print(num1)
-
-# # project-04
-# # cels=int(input("Enter your cels: "))
-# # fehrn=cels*(9/5)+32
-# # # print(fehrn)
-
-# # # project-05
-# # import random
-# # count=0
-# # for i in range(1,4):
-# #    num1=random.randint(1,100)
-# #    count+=num1
-# # print(count)
+# # # # project-05
+# # # import random
+# # # count=0
+# # # for i in range(1,4):
+# # #    num1=random.randint(1,100)
+# # #    count+=num1
+# # # print(count)
 
 
